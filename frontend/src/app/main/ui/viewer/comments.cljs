73a35354a559b4957e69a639ca7e7255bee3ad43
alonso.torres <alonso.torres@kaleidos.net>
2022-06-07 15:49:49 +0200

    :sparkles: Fix comments for nested frames

--- frontend/src/app/main/ui/viewer/comments.cljs
+++ frontend/src/app/main/ui/viewer/comments.cljs
@@ -6,6 +6,7 @@
 
 (ns app.main.ui.viewer.comments
   (:require
+   [app.common.geom.shapes :as gsh]
    [app.common.geom.matrix :as gmt]
    [app.common.geom.point :as gpt]
    [app.main.data.comments :as dcm]
@@ -62,13 +63,6 @@
         [:span.label (tr "labels.hide-resolved-comments")]]]]]))
 
 
-(defn- frame-contains?
-  [{:keys [x y width height]} {px :x py :y}]
-  (let [x2 (+ x width)
-        y2 (+ y height)]
-    (and (<= x px x2)
-         (<= y py y2))))
-
 (def threads-ref
   (l/derived :comment-threads st/state))
 
@@ -80,11 +74,11 @@
   (let [profile     (mf/deref refs/profile)
         threads-map (mf/deref threads-ref)
 
-        modifier1   (-> (gpt/point (:x frame) (:y frame))
-                        (gpt/negate)
-                        (gmt/translate-matrix))
+        frame-corner (-> frame :points gsh/points->selrect gpt/point)
+        modifier1   (-> (gmt/matrix)
+                        (gmt/translate (gpt/negate frame-corner)))
 
-        modifier2   (-> (gpt/point (:x frame) (:y frame))
+        modifier2   (-> (gpt/point frame-corner)
                         (gmt/translate-matrix))
 
         cstate      (mf/deref refs/comments-local)
@@ -92,7 +86,7 @@
         threads     (->> (vals threads-map)
                          (dcm/apply-filters cstate profile)
                          (filter (fn [{:keys [position]}]
-                                   (frame-contains? frame position))))
+                                   (gsh/has-point? frame position))))
 
         on-bubble-click
         (mf/use-callback
